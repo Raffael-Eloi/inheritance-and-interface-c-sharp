afb606b6a25aa0f7a90c1562a8e7c9643f67775a
Raffael Eloi <raffaeleloi121@gmail.com>
2023-01-28 16:19:52 -0300

    add scenario When_Get_A_Bonus_The_Value_Should_Be_Twenty_Percent_Of_The_Salary

--- bytebank_ADM.Test/AssistantShould.cs
+++ bytebank_ADM.Test/AssistantShould.cs
@@ -37,5 +37,32 @@ namespace bytebank_ADM.Tests
 
             #endregion
         }
+
+        [Test]
+        public void When_Get_A_Bonus_The_Value_Should_Be_Twenty_Percent_Of_The_Salary()
+        {
+            #region Arrange
+
+            var name = "Jhon";
+            var document = "123456789";
+
+            Employee jhon = new Assistant(name, document);
+
+            #endregion
+
+            #region Act
+
+            double bonus = jhon.GetBonus();
+
+            #endregion
+
+            #region Assert
+
+            var expectedBonus = jhon.Salary * 0.2;
+
+            Assert.That(bonus, Is.EqualTo(expectedBonus));
+
+            #endregion
+        }
     }
 }
