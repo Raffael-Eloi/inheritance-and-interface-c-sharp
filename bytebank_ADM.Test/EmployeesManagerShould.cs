1e7f23d173b294dcff9f8b27baccf4ef9e70cfff
Raffael Eloi <raffaeleloi121@gmail.com>
2023-02-20 15:52:57 -0300

    add list manipulation

---
+++ bytebank_ADM.Test/EmployeesManagerShould.cs
@@ -0,0 +1,190 @@
+﻿using bytebank_ADM.Employees;
+using bytebank_ADM.Utility;
+
+namespace bytebank_ADM.Test
+{
+    [TestFixture]
+    public class EmployeesManagerShould
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private static Employee CreateAssistantEmployee()
+        {
+            return new Assistant("Jhon", "1234567");
+        }
+
+        private static Employee CreateDirectorEmployee()
+        {
+            return new Director("Bernard", "2345678");
+        }
+
+        private static Employee CreateAccountManagerEmployee()
+        {
+            return new AccountManager("Alice", "3456789");
+        }
+
+        [Test]
+        public void When_Register_An_Employee_Get_The_Bonus()
+        {
+            #region Arrange
+
+            Employee jhon = CreateAssistantEmployee();
+            Employee bernard = CreateDirectorEmployee();
+            Employee alice = CreateAccountManagerEmployee();
+
+            #endregion
+
+            #region Act
+
+            var employeesManager = new EmployeesManager();
+
+            employeesManager.Register(jhon);
+            employeesManager.Register(bernard);
+            employeesManager.Register(alice);
+
+            #endregion
+
+            #region Assert
+
+            var expectedBonus = jhon.GetBonus() + bernard.GetBonus() + alice.GetBonus();
+
+            Assert.That(employeesManager.BonusAmount, Is.EqualTo(expectedBonus));
+
+            #endregion
+        }
+
+        [Test]
+        public void When_Register_The_Employee_Should_Be_Added_To_The_Employees_List()
+        {
+            #region Arrange
+
+            Employee jhon = CreateAssistantEmployee();
+            Employee alice = CreateAccountManagerEmployee();
+
+            #endregion
+
+            #region Act
+
+            var employeesManager = new EmployeesManager();
+
+            employeesManager.Register(jhon);
+            employeesManager.Register(alice);
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(employeesManager.Employees.Count(), Is.EqualTo(2));
+            Assert.That(employeesManager.Employees.First(), Is.EqualTo(jhon));
+            Assert.That(employeesManager.Employees.Last(), Is.EqualTo(alice));
+
+            #endregion
+        }
+
+        [Test]
+        public void When_Clear_The_Employees_List_Should_Be_Empty()
+        {
+            #region Arrange
+
+            Employee jhon = CreateAssistantEmployee();
+            Employee alice = CreateAccountManagerEmployee();
+
+            #endregion
+
+            #region Act
+
+            var employeesManager = new EmployeesManager();
+
+            employeesManager.Register(jhon);
+            employeesManager.Register(alice);
+
+            employeesManager.ClearEmployeesList();
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(employeesManager.Employees.Count, Is.EqualTo(0));
+
+            #endregion
+        }
+
+        [Test]
+        public void Return_True_When_Employee_Exist_In_List_Of_Employees()
+        {
+            #region Arrange
+
+            Employee jhon = CreateAssistantEmployee();
+
+            #endregion
+
+            #region Act
+
+            var employeesManager = new EmployeesManager();
+
+            employeesManager.Register(jhon);
+
+            bool employeeExist= employeesManager.IsEmployeeAdded(jhon);
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(employeeExist, Is.True);
+
+            #endregion
+        }
+
+        [Test]
+        public void Return_False_When_Employee_Does_Not_Exist_In_List_Of_Employees()
+        {
+            #region Arrange
+
+            Employee jhon = CreateAssistantEmployee();
+
+            #endregion
+
+            #region Act
+
+            var employeesManager = new EmployeesManager();
+
+            bool employeeExist = employeesManager.IsEmployeeAdded(jhon);
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(employeeExist, Is.False);
+
+            #endregion
+        }
+
+        [Test]
+        public void Remove_Employee_When_Exist()
+        {
+            #region Arrange
+
+            Employee jhon = CreateAssistantEmployee();
+
+            #endregion
+
+            #region Act
+
+            var employeesManager = new EmployeesManager();
+
+            employeesManager.RemoveEmployee(jhon);
+
+            #endregion
+
+            #region Assert
+
+            Assert.That(employeesManager.Employees.Count, Is.EqualTo(0));
+
+            #endregion
+        }
+    }
+}
---
+++ bytebank_ADM/Utility/EmployeesManager.cs
@@ -0,0 +1,50 @@
+﻿using bytebank_ADM.Employees;
+
+namespace bytebank_ADM.Utility
+{
+    public class EmployeesManager
+    {
+        public double BonusAmount { get; private set; }
+
+        public IList<Employee> Employees { get; private set; }
+
+        public EmployeesManager()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public void Register(Employee employee)
+        {
+            BonusAmount += employee.GetBonus();
+            Employees.Add(employee);
+        }
+
+        public void ShowEmployeesRegistered()
+        {
+            foreach (Employee employee in Employees)
+            {
+                Console.WriteLine($"Employee registered: {Employees.IndexOf(employee)} - Name: {employee.Name}");
+            }
+
+            Console.WriteLine($"Total of employees registered: {Employees.Count}");
+        }
+
+        public void ClearEmployeesList()
+        {
+            Employees.Clear();
+        }
+
+        public bool IsEmployeeAdded(Employee jhon)
+        {
+            return Employees.Contains(jhon);
+        }
+
+        public void RemoveEmployee(Employee jhon)
+        {
+            if (IsEmployeeAdded(jhon))
+            {
+                Employees.Remove(jhon);
+            }
+        }
+    }
+}
